bc1bc298cea3e46b5dc58abf8f5654b8cdea00f5
Nicholas Konzen <nicholaskonzen@gmail.com>
2020-07-28 17:35:27 -0700

    Updated the README and added comments to the script.js

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# homework_4
+# Javascript Quiz #
+
+[How to Play](#How-to-Play)\
+[Features](#features)
+
+This javascript quiz was created to test my own javascript skills while at the same time testing my knowledge of javascript trivia. Although I created the quiz for myself, I've made it very easy to change or add questions to the quiz. The quiz works by running you through a series of multiple choice questions while running a countdown from 75 seconds. Your overall score is based on how many seconds you have left once the quiz is completed.
+
+![A screenshot of the start screen](./screenshots/Screen Shot 2020-07-28 at 5.17.05 PM.png)
+
+### How to Play ###
+
+1. To begin the quiz, press the 'Begin Quiz' button
+1. You'll be presented with a multiple choice question. Read through all the possible answers and click the one you believe to be correct.
+1. If you answered correctly, you'll be moved to the next question
+1. If you answered incorrectly, you'll be moved to the next question but a predetermined amount of seconds will be removed from the timer
+1. If you answer too many questions incorrectly or take too long to finish the quiz, the game will end and you will not be allowed to log your score.
+1. If you answer enough questions correctly before the clock runs up, you will be prompted to log your score with your intitials 
+1. While the quiz is not running, you may click the 'View High Scores' button in the top left corner to view the logged high scores
+1. You may also clear the high scores at any time
+
+## Features ##
+
+* A versatile toggle function that can be used for testing the program
+  * To use, enter the toggle() function with the argument being the id of the intended screen you are trying to toggle
+* A High Scores table that orders itself from the top down by score
+* A set of questions that can be added to or manipulated easily 
+  * To add a questions, copy the last section tag with the class 'question' and paste it below said section tag
+  * change the id to whatever you prefer
+  * Update the question and possible to answers to anything you like
+  * Make sure the onClick attribute outputs the correct argument correspondant to the correct answer
+
+---
+Copyright (c) 2020 Konzen, Nicholas
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- script.js
+++ script.js
@@ -1,98 +1,132 @@
+// Main Screen Variables
 const startScreen = document.querySelector('#startScreen');
 const gameOverScreen = document.querySelector('#gameOverScreen');
 const endScreen = document.querySelector('#endScreen');
+const highScoresScreen = document.querySelector('#highScores');
 
-// grab each question by using questions[i]
+// All elements with the class 'question' will be sorted into an array
 const questions = document.getElementsByClassName('question');
 
+// Input and Display Data
 const timeDisplay = document.querySelector('#timeDisplaySpan');
 const viewHighScores = document.querySelector('#viewHighScores');
 const initialsInput = document.querySelector('#initialsInput');
-const highScoresScreen = document.querySelector('#highScores');
 const highScoresTable = document.querySelector('#highScoresTable');
 
+// Button Variables
 const startButton = document.querySelector('#startButton');
 const tryAgainButton = document.querySelector('#tryAgainButton');
 const submitButton = document.querySelector('#submitButton');
 const goBackButton = document.querySelector('#goBack');
 const clearHighScoresButton = document.querySelector('#clearHighScores');
 
+// Default values for global variables
 let secondsLeft = 75;
-
-timeDisplaySpan.textContent = secondsLeft;
 let finished = false;
 let score;
 let onQuestion;
 let currentScreen = startScreen;
 
+// Display proper secondsLeft variable 
+timeDisplaySpan.textContent = secondsLeft;
+
+// Clear the highScoresObj as a precaution
 let highScoresObj = {}
 
+// to avoid setting the highScoresObj to 'null', this check is put in place
 if (JSON.parse(localStorage.getItem('highScores')) !== null) {
+    // Sets the highScoresObj to the locally stored object
     highScoresObj = JSON.parse(localStorage.getItem('highScores'));
 };
 
+// toggle function declaration
 function toggle(screenName) {
 
-
+    // When either of these screens are toggled, finish the quiz and empty the input
     if (screenName === endScreen || screenName === highScoresScreen) {
         finished = true;
         initialsInput.value = ''
     }
 
+    // When the start screen is toggled, reset the time display
     if (screenName === startScreen) {
         timeDisplay.textContent = '75';
     }
 
+    // Checks if the screen is already displayed
     if (getComputedStyle(screenName).display === 'block' && screenName !== highScoresScreen) {
         console.log(screenName.id + ' is already displayed')
     } else if (getComputedStyle(screenName).display === 'none') {
 
+        // hides all questions
         for (i = 0; i < questions.length; i++) {
             questions[i].style.display = 'none';
         }
 
+        // hides all non-question screens
         startScreen.style.display = 'none';
         gameOverScreen.style.display = 'none';
         endScreen.style.display = 'none';
         highScoresScreen.style.display = 'none';
 
+        // displays the toggled screen
         screenName.style.display = 'block';
         console.log('Displaying ' + screenName.id);
-        currentScreen = screenName.id;
+        currentScreen = screenName; // used for viewHighScores() functionality
 
     }
 
-    if (screenName === highScoresScreen) {
+    // creates and displays the highScores table
+    if (screenName === highScoresScreen) { // if the toggled screen is highScoresScreen
 
-        let highScoresArray = Object.values(highScoresObj).sort(function(a,b){
-            return b-a;
-        });
+        /* 
+            The next five groups of code sorts the highScoresObj key/value pairs by score
+            The key/value pairs are also sorted from the top down
+        */
 
-        let newObjFunc = (object, value) => {
-            return Object.keys(object).find(key => object[key] === value)
-        }
+            // sorts the scores in highScoresObj into an ordered array
+            let highScoresArray = Object.values(highScoresObj).sort(function(a,b){
+                return b-a;
+            });
 
-        let newObj = {};
+            // a function that returns a key,value pairs based on the given object and value
+            let newObjFunc = (object, value) => {
+                return Object.keys(object).find(key => object[key] === value)
+            }
 
-        highScoresArray.forEach(value => {
-            newObj[newObjFunc(highScoresObj, value)] = value;
-        })
+            // create an empty object
+            let newObj = {};
+
+            // for every score in highScoresArray
+            highScoresArray.forEach(value => {
+                // adds the return value of newObjFunc to newObj
+                newObj[newObjFunc(highScoresObj, value)] = value;
+            })
         
-        highScoresObj = newObj;
+            // Copies newObj into highScoresObj
+            highScoresObj = newObj;
 
+        // sets the highScores item in local storage to the new, organized highScores object
         localStorage.setItem('highScores', JSON.stringify(highScoresObj));
 
+        // clears the existing highScores table
         highScoresTable.innerHTML = ''
 
-        for (let key in highScoresObj) {
+        // creates the new highScores table
+        for (let key in highScoresObj) { // for every key in the highScores object
+            
+            // creates the necessary table row and table data elements
             let newRow = document.createElement('tr');
             let initialsData = document.createElement('td');
             let scoreData = document.createElement('td');
 
+            // displays the initials in the proper cell
             initialsData.textContent = key;
 
+            // displays the score in the proper cell
             scoreData.textContent = highScoresObj[key]
 
+            // adds the table row and table data to the document
             highScoresTable.appendChild(newRow);
             newRow.appendChild(initialsData);
             newRow.appendChild(scoreData);
@@ -100,65 +134,76 @@ function toggle(screenName) {
     }
 }
 
-
+// timer function declaration
 function startTimer() {
+    // sets the secondsLeft to its default value
     secondsLeft = 75;
+    // starts the timer
     timer = setInterval(function () {
+        // checks if the quiz has finished
         if (finished === false) {
+            // Prevents the timer from going below 0
             if (secondsLeft > 1) {
                 secondsLeft--;
                 timeDisplaySpan.textContent = secondsLeft;
             } else {
                 secondsLeft--;
+                // If the time goes below zero, it sets the time to zero
                 if (secondsLeft < 0) {
                     secondsLeft = 0;
                 }
                 timeDisplaySpan.textContent = secondsLeft;
+                // stops the timer
                 clearInterval(timer);
-                toggle(gameOverScreen);
+                toggle(gameOverScreen); // game over man
             }
         } else {
+            // If the quiz is finished, stop the timer
             clearInterval(timer);
             toggle(endScreen);
-            score = secondsLeft;
+            score = secondsLeft; // sets the score to the time left
         };
 
     }, 1000)
 }
 
 
-// Beginning of nextQuestion()
+// nextQuestion function declared
 function nextQuestion(status) {
 
+    // onQuestion is used to toggle the correct screen
     onQuestion++;
 
-    // Beginning of correctDisplay()
+    // correctDisplay function declaration
     function correctDisplay() {
 
-        if (questions[onQuestion] !== undefined) {
+        // Displays 'correct' on the proper screen
+        if (questions[onQuestion] !== undefined) { // checks if the questions exists
 
-            console.log('correct');
-
+            // creates the 'correct' display
             let correctDisplayEl = document.createElement('h5');
             correctDisplayEl.textContent = 'Correct!'
             correctDisplayEl.setAttribute('class', 'correctDisplay');
 
+            // adds the correct display to the question div
             questions[onQuestion].append(correctDisplayEl);
 
+            // removes the correct display from the document after one second
             setTimeout(function () {
                 document.querySelector('.correctDisplay').remove();
             }, 1000)
 
-        } else {
+        } else { // if the question doesn't exist
 
-            console.log('correct');
-
+            // creates the correct display
             let correctDisplayEl = document.createElement('h5');
             correctDisplayEl.textContent = 'Correct!';
             correctDisplayEl.setAttribute('class', 'correctDisplay');
 
+            // adds the correct display to the end screen div
             endScreen.append(correctDisplayEl);
 
+            // removes the correct display from the document after one second
             setTimeout(function () {
                 document.querySelector('.correctDisplay').remove();
             }, 1000)
@@ -166,51 +211,59 @@ function nextQuestion(status) {
         }
 
     }
-    // End of correctDisplay();
 
-    // Beginning of wrongDisplay();
+    // wrongDisplay function declared 
     function wrongDisplay() {
 
-        if (questions[onQuestion] !== undefined) {
+        // Displays 'correct' on the proper screen
+        if (questions[onQuestion] !== undefined) { // checks if the questions exists
 
-            console.log('wrong');
+            // subtracts time from the clock based on how many questions there are
+            secondsLeft = secondsLeft - Math.floor(75 / (questions.length - 1));
 
-            secondsLeft = secondsLeft - Math.floor(75 / questions.length);
-
+            // If the timer goes below zero, this corrects that
             if (secondsLeft < 0) {
                 secondsLeft = 0;
             }
 
+            // updates the time display
             timeDisplaySpan.textContent = secondsLeft;
 
+            // creates the 'wrong' display
             let wrongDisplayEl = document.createElement('h5');
             wrongDisplayEl.textContent = 'Wrong!'
             wrongDisplayEl.setAttribute('class', 'wrongDisplay');
 
+            // adds the 'wrong' display to the question div
             questions[onQuestion].append(wrongDisplayEl);
 
+            // removes the 'wrong' display after one second
             setTimeout(function () {
                 document.querySelector('.wrongDisplay').remove();
             }, 1000)
 
-        } else {
+        } else { // if the question does not exist
 
-            console.log('wrong');
+            // subtracts time from the clock based on how many questions there are
+            secondsLeft = secondsLeft - Math.floor(75 / (questions.length - 1));
 
-            secondsLeft = secondsLeft - Math.floor(75 / questions.length);
-
+            // If the timer goes below zero, this corrects that
             if (secondsLeft < 0) {
                 secondsLeft = 0;
             }
 
+            // updates the time display
             timeDisplaySpan.textContent = secondsLeft;
 
+            // creates the 'wrong' display
             let wrongDisplayEl = document.createElement('h5');
             wrongDisplayEl.textContent = 'Wrong!'
             wrongDisplayEl.setAttribute('class', 'wrongDisplay');
 
+            // adds the 'wrong' display to the end screen div
             endScreen.append(wrongDisplayEl);
 
+            // removes the 'wrong' display after one second
             setTimeout(function () {
                 document.querySelector('.wrongDisplay').remove();
             }, 1000)
@@ -218,57 +271,63 @@ function nextQuestion(status) {
         }
 
     }
-    // End of wrongDisplay();
 
-
+    // status is the input given by the html buttons
     if (status === 'wrong') {
         wrongDisplay();
     } else if (status === 'correct') {
         correctDisplay();
     }
 
+    // toggles proper screen
     if (questions[onQuestion] !== undefined) {
         toggle(questions[onQuestion]);
     } else if (questions[onQuestion] === undefined && secondsLeft !== 0) {
         toggle(endScreen)
-    } else {
+    } else { // if the timer hits zero, you get sent to the game over screen, not the end screen
         toggle(gameOverScreen)
     }
 
 }
-// End of nextQuestion;
 
-
+// startQuiz function declared
 function startQuiz() {
+    // creates and updates the necessary variables
     let timer;
     finished = false;
-    startTimer();
-
     onQuestion = -1;
 
+    // starts the timer
+    startTimer();
+
+    // goes to the next question
     nextQuestion();
 
 };
 
+// start button triggers startQuiz()
 startButton.addEventListener('click', startQuiz);
 
+// view high scores button triggers this function
 viewHighScores.addEventListener('click', function (event) {
     event.preventDefault();
     event.stopPropagation();
 
-    if (currentScreen === startScreen || currentScreen === 'endScreen' || currentScreen === 'gameOverScreen') {
+
+    // Does not function if the user is not viewing one of these screens
+    if (currentScreen.id === 'startScreen' || currentScreen.id === 'endScreen' || currentScreen.id === 'gameOverScreen') {
         toggle(highScores);
     }
 });
 
+// submit button triggers this function
 submitButton.addEventListener('click', function (event) {
     event.preventDefault();
     event.stopPropagation();
 
-    console.log(initialsInput.value);
-    
-
+    // If the user doesn't enter anything
     if (initialsInput.value === '') {
+        // lets the user know they need to enter something
         let enterSomething = document.createElement('h5');
         enterSomething.textContent = 'Your initials cannot be blank!'
         enterSomething.setAttribute('class', 'correctDisplay');
@@ -276,15 +335,23 @@ submitButton.addEventListener('click', function (event) {
         setTimeout(function () {
             document.querySelector('.correctDisplay').remove();
         }, 1300)
+
+        // break out of the function
         return;
     }
 
-    if (highScoresObj[initialsInput.value] === undefined) {
+    // Disallows reusing initials
+    if (highScoresObj[initialsInput.value] === undefined) { // if the entered intials don't exist
+
+        // adds the users initials and score to the highScores object as a key,value pair
         highScoresObj[(initialsInput.value).toUpperCase()] = score;
         toggle(highScores);
-    } else if (highScoresObj[initialsInput.value] !== undefined) {
+
+    } else if (highScoresObj[initialsInput.value] !== undefined) { // if the entered intials already exist
+
+        // let the user know they have already been claimed
         let differentName = document.createElement('h5');
-        differentName.textContent = 'This name has already been claimed! Try entering a new one!'
+        differentName.textContent = 'Those intials have already been claimed! Try entering a new one!'
         differentName.setAttribute('class', 'wrongDisplay');
 
         endScreen.append(differentName);
@@ -292,31 +359,43 @@ submitButton.addEventListener('click', function (event) {
         setTimeout(function () {
             document.querySelector('.wrongDisplay').remove();
         }, 4000)
+
+        // break out of the function
+        return;
     }
 
 });
 
+// go back button triggers this function
 goBackButton.addEventListener('click', function (event) {
     event.preventDefault();
     event.stopPropagation();
 
+    // toggles the start screen and resets secondsLeft
     toggle(startScreen);
     secondsLeft = 75;
 });
 
+// try again button
 tryAgainButton.addEventListener('click', function (event) {
     event.preventDefault();
     event.stopPropagation();
 
+    // toggles the start screen and resets secondsLeft
     toggle(startScreen);
     secondsLeft = 75;
 });
 
+// clear high scores button triggers this function
 clearHighScoresButton.addEventListener('click', function (event) {
     event.preventDefault();
     event.stopPropagation();
+
+    // clears the local storage
     localStorage.clear();
+    // clears the highScores object
     highScoresObj = {}
+    // refreshes the highScores screen
     toggle(highScores);
 });
 
